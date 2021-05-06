f6035217580380e3ced9fd7988b57dc2244680dc
ragdoll <museopkim0214@gmail.com>
2021-05-06 22:47:32 +0900

    Initial commit

---
+++ src/main/java/com/careerzip/CareerZipApplication.java
@@ -0,0 +1,13 @@
+package com.careerzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerZipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/careerzip/CareerZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerZipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
