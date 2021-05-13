abc61fe32d50d172c913ede39a7714386473b43b
ragdoll <museopkim0214@gmail.com>
2021-05-13 11:23:13 +0900

    Feat: AccountNotFoundException 추가
    Account 못 찾았을 경우 구체적인 예외를 던지기 위해 추가 및 적용
    
    [#10]

--- src/main/java/com/careerzip/domain/account/service/AccountService.java
+++ src/main/java/com/careerzip/domain/account/service/AccountService.java
@@ -2,7 +2,7 @@ package com.careerzip.domain.account.service;
 
 import com.careerzip.domain.account.entity.Account;
 import com.careerzip.domain.account.repository.AccountRepository;
-import com.careerzip.global.error.exception.EntityNotFoundException;
+import com.careerzip.global.error.exception.entity.AccountNotFoundException;
 import com.careerzip.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ public class AccountService {
 
     public String issueJwtToken(String authorizationHeader) {
         Long accountId = jwtTokenProvider.parsePreAuthToken(authorizationHeader);
-        Account account = accountRepository.findById(accountId).orElseThrow(EntityNotFoundException::new);
+        Account account = accountRepository.findById(accountId).orElseThrow(AccountNotFoundException::new);
         return jwtTokenProvider.issueJwtToken(account);
     }
 }
