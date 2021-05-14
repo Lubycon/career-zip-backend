0ecf971b96e77c20fa4b11c9423829268806bdc4
ragdoll <museopkim0214@gmail.com>
2021-05-14 18:11:34 +0900

    Feat: JWT 필터 기본 예외 처리 로직 추가
    [#15]

--- src/main/java/com/careerzip/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/careerzip/security/filter/JwtAuthorizationFilter.java
@@ -2,13 +2,19 @@ package com.careerzip.security.filter;
 
 import com.careerzip.domain.account.entity.Account;
 import com.careerzip.domain.account.repository.AccountRepository;
+import com.careerzip.global.error.exception.JwtValidationException;
 import com.careerzip.global.error.exception.entity.AccountNotFoundException;
+import com.careerzip.global.error.response.ErrorCode;
+import com.careerzip.global.error.response.ErrorResponse;
 import com.careerzip.global.jwt.JwtTokenProvider;
 import com.careerzip.global.jwt.claims.AccountClaims;
 import com.careerzip.security.oauth.dto.OAuthAccount;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,29 +32,58 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final AccountRepository accountRepository;
+    private final ObjectMapper objectMapper;
 
-    private JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider, AccountRepository accountRepository) {
+    @Builder
+    private JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider, AccountRepository accountRepository,
+                                   ObjectMapper objectMapper) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.accountRepository = accountRepository;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String authorizationHeader = response.getHeader(HttpHeaders.AUTHORIZATION);
-        AccountClaims accountClaims = jwtTokenProvider.parseJwtToken(authorizationHeader);
+        try {
+            String authorizationHeader = response.getHeader(HttpHeaders.AUTHORIZATION);
+            AccountClaims accountClaims = jwtTokenProvider.parseJwtToken(authorizationHeader);
 
-        Account account = accountRepository.findById(accountClaims.getId()).orElseThrow(AccountNotFoundException::new);
-        OAuthAccount loginAccount = OAuthAccount.from(account);
+            Account account = accountRepository.findById(accountClaims.getId()).orElseThrow(AccountNotFoundException::new);
+            OAuthAccount loginAccount = OAuthAccount.from(account);
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(loginAccount, jwtTokenProvider.extractToken(authorizationHeader),
-                                                                                loginAccount.getAuthorities());
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        filterChain.doFilter(request, response);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(loginAccount, jwtTokenProvider.extractToken(authorizationHeader),
+                                                                                    loginAccount.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+        } catch (JwtValidationException exception) {
+            // TODO: 에러 처리 중복 로직 정리
+            log.error("handleJwtValidationException", exception);
+            ErrorCode errorCode =  exception.getErrorCode();
+            String errorResponse = objectMapper.writeValueAsString(ErrorResponse.from(errorCode));
+            response.getWriter().print(errorResponse);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(errorCode.getStatusCode());
+            response.getWriter().flush();
+            response.getWriter().close();
+        } catch (Exception exception) {
+            log.error("Exception", exception);
+            ErrorCode errorCode =  ErrorCode.INTERNAL_SERVER_ERROR;
+            String errorResponse = objectMapper.writeValueAsString(ErrorResponse.from(errorCode));
+            response.getWriter().print(errorResponse);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(errorCode.getStatusCode());
+            response.getWriter().flush();
+            response.getWriter().close();
+        }
     }
 
-    public static JwtAuthorizationFilter of(JwtTokenProvider jwtTokenProvider, AccountRepository accountRepository) {
-        return new JwtAuthorizationFilter(jwtTokenProvider, accountRepository);
+    public static JwtAuthorizationFilter of(JwtTokenProvider jwtTokenProvider, AccountRepository accountRepository,
+                                            ObjectMapper objectMapper) {
+        return JwtAuthorizationFilter.builder()
+                                     .jwtTokenProvider(jwtTokenProvider)
+                                     .accountRepository(accountRepository)
+                                     .objectMapper(objectMapper)
+                                     .build();
     }
 }
