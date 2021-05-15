aa36a8d9513a538f3b323c47a40a10721a3acd3f
ragdoll <museopkim0214@gmail.com>
2021-05-15 12:55:49 +0900

    Feat: JWT 필터 등록 및 시큐리티 CORS 설정
    [#15]

--- src/main/java/com/careerzip/security/SecurityConfig.java
+++ src/main/java/com/careerzip/security/SecurityConfig.java
@@ -1,16 +1,31 @@
 package com.careerzip.security;
 
+import com.careerzip.domain.account.repository.AccountRepository;
+import com.careerzip.global.jwt.JwtTokenProvider;
+import com.careerzip.security.filter.JwtAuthorizationFilter;
 import com.careerzip.security.oauth.handler.CustomAuthenticationEntryPoint;
 import com.careerzip.security.oauth.handler.CustomAuthenticationSuccessHandler;
 import com.careerzip.security.oauth.service.CustomOAuth2UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -19,6 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AccountRepository accountRepository;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,7 +57,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .successHandler(customAuthenticationSuccessHandler);
 
-        http.addFilterBefore(characterEncodingFilter(), CsrfFilter.class);
+        http.cors(withDefaults());
+
+        http.addFilterBefore(characterEncodingFilter(), CsrfFilter.class)
+            .addFilterBefore(JwtAuthorizationFilter.of(jwtTokenProvider, accountRepository, objectMapper), UsernamePasswordAuthenticationFilter.class);
 
         http.exceptionHandling()
             .authenticationEntryPoint(customAuthenticationEntryPoint);
@@ -50,5 +71,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         encodingFilter.setEncoding("UTF-8");
         encodingFilter.setForceEncoding(true);
         return encodingFilter;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://career-zip.com", "http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setExposedHeaders(Collections.singletonList(HttpHeaders.AUTHORIZATION));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
