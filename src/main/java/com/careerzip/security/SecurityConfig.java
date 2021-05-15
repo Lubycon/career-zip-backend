7d87d9b9a54673b24ab88ae9ffb9f05713dea8b8
ragdoll <museopkim0214@gmail.com>
2021-05-15 23:56:01 +0900

    Fix: 임시 토큰 검증 API 시큐리티 인증 필터를 타지 않도록 수정
    [#3]

--- src/main/java/com/careerzip/security/SecurityConfig.java
+++ src/main/java/com/careerzip/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -39,13 +40,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final ObjectMapper objectMapper;
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/v1/accounts/auth");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         // NOTICE: 개발의 편의성을 위해 csrf 옵션을 종료 합니다.
         http.csrf().disable()
             .httpBasic().disable();
 
         http.authorizeRequests()
-            .antMatchers(HttpMethod.POST, "/v1/accounts/auth").permitAll()
             .anyRequest().authenticated();
 
         http.sessionManagement()
