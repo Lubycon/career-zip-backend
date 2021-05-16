734700a6ba77ea05fdc244a2c98fd476ee6a56b1
ragdoll <museopkim0214@gmail.com>
2021-05-17 03:47:40 +0900

    Chore: QuerydslConfig 추가
    [#17]

--- src/test/java/com/careerzip/testconfig/base/BaseRepositoryTest.java
+++ src/test/java/com/careerzip/testconfig/base/BaseRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.careerzip.testconfig.base;
 
 import com.careerzip.config.jpa.JpaConfig;
+import com.careerzip.testconfig.jpa.TestQuerydslConfig;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -8,7 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @AutoConfigureTestDatabase
 @ActiveProfiles("test")
-@Import({JpaConfig.class})
+@Import({JpaConfig.class, TestQuerydslConfig.class})
 @DataJpaTest
 public class BaseRepositoryTest {
 
