0c4efb213477e2582bafd7e354e7d7b373c5fc63
ragdoll <museopkim0214@gmail.com>
2021-05-16 22:31:44 +0900

    Feat: GlobalExceptionHandler - MethodArgumentNotValidException 등록
    [#3]

--- src/main/java/com/careerzip/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/careerzip/global/error/GlobalExceptionHandler.java
@@ -8,12 +8,21 @@ import com.careerzip.global.error.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("handleMethodArgumentNotValidException", exception);
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_ERROR;
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode, exception.getBindingResult());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatusCode()));
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     protected ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException exception) {
