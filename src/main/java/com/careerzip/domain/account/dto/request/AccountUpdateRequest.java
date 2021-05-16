b72048bbb3f4599e23e54f024dc1d2afe67774ee
ragdoll <museopkim0214@gmail.com>
2021-05-16 23:25:05 +0900

    Feat: Account 수정 DTO - Email, Name validation 추가
    [#3]

--- src/main/java/com/careerzip/domain/account/dto/request/AccountUpdateRequest.java
+++ src/main/java/com/careerzip/domain/account/dto/request/AccountUpdateRequest.java
@@ -2,18 +2,24 @@ package com.careerzip.domain.account.dto.request;
 
 
 import lombok.Getter;
-import org.jetbrains.annotations.Nullable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 public class AccountUpdateRequest {
 
-    @Nullable
+    @NotEmpty
+    @Size(min = 1, max = 16, message = "닉네임은 1자 ~ 16자 사이 입니다.")
     private String name;
 
-    @Nullable
+    @NotNull
+    @Email(message = "유효한 이메일 주소가 아닙니다. 다시 입력 해주세요.")
     private String email;
 
-    AccountUpdateRequest(@Nullable String name, @Nullable String email) {
+    AccountUpdateRequest( String name,  String email) {
         this.name = name;
         this.email = email;
     }
