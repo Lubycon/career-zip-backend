c80dd0dc3bcaa2c85dffcc40453198fbfa195c5e
ragdoll <museopkim0214@gmail.com>
2021-05-20 17:03:43 +0900

    Feat: TemplateQuestion 누락 되어 있던 연관관계 매핑
    [#21]

--- src/main/java/com/careerzip/domain/templatequestion/entity/TemplateQuestion.java
+++ src/main/java/com/careerzip/domain/templatequestion/entity/TemplateQuestion.java
@@ -1,6 +1,9 @@
 package com.careerzip.domain.templatequestion.entity;
 
+import com.careerzip.domain.question.entity.Question;
+import com.careerzip.domain.template.entity.Template;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +18,23 @@ public class TemplateQuestion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "template_question_id", nullable = false)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "template_id", nullable = false)
+    private Template template;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(name = "order")
+    private Integer order;
+
+    @Builder
+    private TemplateQuestion(Long id, Template template, Question question, Integer order) {
+        this.id = id;
+        this.template = template;
+        this.question = question;
+        this.order = order;
+    }
 }
