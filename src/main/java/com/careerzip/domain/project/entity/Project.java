268ef04632ec15d311aceb8f06fffe42df8517e7
ragdoll <museopkim0214@gmail.com>
2021-05-27 03:59:04 +0900

    Feat: Project 엔티티 요구사항에 맞게 필드 추가
    [#21]

--- src/main/java/com/careerzip/domain/project/entity/Project.java
+++ src/main/java/com/careerzip/domain/project/entity/Project.java
@@ -21,14 +21,43 @@ public class Project {
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "aim")
+    private String aim;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "main_business")
+    private String mainBusiness;
+
+    @Column(name = "participants_count")
+    private Integer participantsCount;
+
+    @Column(name = "contribution")
+    private Double contribution;
+
+    @Column(name = "team_members")
+    private String teamMembers;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
     @Builder
-    private Project(Long id, String title, Account account) {
+    private Project(Long id, String title, String description, String aim, String role, String mainBusiness,
+                    Integer participantsCount, Double contribution, String teamMembers, Account account) {
         this.id = id;
         this.title = title;
+        this.description = description;
+        this.aim = aim;
+        this.role = role;
+        this.mainBusiness = mainBusiness;
+        this.participantsCount = participantsCount;
+        this.contribution = contribution;
+        this.teamMembers = teamMembers;
         this.account = account;
     }
 }
