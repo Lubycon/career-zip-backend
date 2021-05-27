a7af1d538daa5ea057c0aa7d8d9c36badf93a5f1
ragdoll <museopkim0214@gmail.com>
2021-05-28 00:46:53 +0900

    Feat: ArchiveService - 복잡한 ArchiveDetail DTO 반환 로직 검증
    [#21]

--- src/test/java/com/careerzip/domain/archive/service/ArchiveServiceTest.java
+++ src/test/java/com/careerzip/domain/archive/service/ArchiveServiceTest.java
@@ -3,8 +3,10 @@ package com.careerzip.domain.archive.service;
 import com.careerzip.domain.account.entity.Account;
 import com.careerzip.domain.account.repository.AccountRepository;
 import com.careerzip.domain.archive.dto.response.archivedetailresponse.ArchiveDetailResponse;
+import com.careerzip.domain.archive.dto.response.archivedetailresponse.ProjectSummary;
 import com.careerzip.domain.archive.dto.response.archivedetailresponse.QuestionWithAnswers;
 import com.careerzip.domain.archive.entity.Archive;
+import com.careerzip.domain.project.service.ProjectService;
 import com.careerzip.domain.questionpaper.entity.QuestionPaper;
 import com.careerzip.domain.archive.dto.response.archivingsresponse.ArchivingsResponse;
 import com.careerzip.domain.archive.repository.ArchiveRepository;
@@ -18,12 +20,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static com.careerzip.testobject.account.AccountFactory.createMember;
 import static com.careerzip.testobject.account.AccountFactory.createOAuthAccountOf;
@@ -50,6 +54,9 @@ class ArchiveServiceTest {
 
     @Mock
     QuestionService questionService;
+
+    @Spy
+    ProjectService projectService;
 
     @Test
     @DisplayName("특정 Account가 작성한 Archiving 리스트를 DTO로 반환하는 테스트")
@@ -87,14 +94,16 @@ class ArchiveServiceTest {
     }
 
     @Test
-    @DisplayName("특정 Account가 작성한 단일 Archiving을 DTO로 반환하는 테스트")
+    @DisplayName("특정 Account가 작성한 단일 Archive를 DTO로 반환하는 테스트")
     void findByTest() {
         // given
         Account account = createMember();
         OAuthAccount loginAccount = createOAuthAccountOf(account);
         List<QuestionWithAnswers> questionWithAnswers = QuestionWithAnswers.listOf(createQuestions(), createAnswers());
         Archive archive = createArchive();
+        QuestionPaper questionPaper = archive.getQuestionPaper();
         Long archivingId = archive.getId();
+        Set<ProjectSummary> selectedProjects = projectService.findSelectedProjectsBy(questionWithAnswers);
 
         // when
         when(accountRepository.findById(loginAccount.getId())).thenReturn(Optional.of(account));
@@ -106,7 +115,10 @@ class ArchiveServiceTest {
         // then
         assertAll(
                 () -> assertThat(response.getArchiveId()).isEqualTo(archive.getId()),
+                () -> assertThat(response.getStartDate()).isEqualTo(questionPaper.getStartDateTime().toLocalDate()),
+                () -> assertThat(response.getEndDate()).isEqualTo(questionPaper.getEndDateTime().toLocalDate()),
                 () -> assertThat(response.getCreatedDateTime()).isEqualTo(archive.getCreatedDateTime()),
+                () -> assertThat(response.getSelectedProjects().size()).isEqualTo(selectedProjects.size()),
                 () -> assertThat(response.getQuestions()).usingRecursiveComparison().isEqualTo(questionWithAnswers)
         );
     }
