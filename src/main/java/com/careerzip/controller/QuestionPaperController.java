04d881adb84bd0af683543f16eef657cdea092d5
ragdoll <museopkim0214@gmail.com>
2021-05-30 18:56:37 +0900

    Feat: QuestionPaperController - 최신 QuestionPaper 조회 API 구현
    [#25]

---
+++ src/main/java/com/careerzip/controller/QuestionPaperController.java
@@ -0,0 +1,23 @@
+package com.careerzip.controller;
+
+import com.careerzip.domain.questionpaper.dto.LatestQuestionPaperResponse;
+import com.careerzip.domain.questionpaper.service.QuestionPaperService;
+import com.careerzip.global.api.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/questionpapers")
+public class QuestionPaperController {
+
+    private final QuestionPaperService questionPaperService;
+
+    @GetMapping
+    public ApiResponse<LatestQuestionPaperResponse> latest() {
+        LatestQuestionPaperResponse questionPaper = questionPaperService.findLatest();
+        return ApiResponse.success(questionPaper);
+    }
+}
