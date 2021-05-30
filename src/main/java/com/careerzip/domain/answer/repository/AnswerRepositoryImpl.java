0a86c83f0eb94345f94016e09af785491d3e9da2
ragdoll <museopkim0214@gmail.com>
2021-05-30 13:18:51 +0900

    Fix: 이전 Answers 조회시 Question이 아닌 QuestionItem에 의존으로 하도록 수정
    새로운 질문지 구성에 따라 Question은 변경될 수도 있으므로 조회 시 QuestionItem에 의존하도록 변경
    
    [#23]

--- src/main/java/com/careerzip/domain/answer/repository/AnswerRepositoryImpl.java
+++ src/main/java/com/careerzip/domain/answer/repository/AnswerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.careerzip.domain.answer.repository;
 import com.careerzip.domain.answer.entity.Answer;
 import com.careerzip.domain.archive.entity.Archive;
 import com.careerzip.domain.question.entity.Question;
+import com.careerzip.domain.questionitem.entity.QuestionItem;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +14,7 @@ import static com.careerzip.domain.answer.entity.QAnswer.answer;
 import static com.careerzip.domain.archive.entity.QArchive.archive;
 import static com.careerzip.domain.project.entity.QProject.project;
 import static com.careerzip.domain.question.entity.QQuestion.question;
+import static com.careerzip.domain.questionitem.entity.QQuestionItem.questionItem;
 
 @RequiredArgsConstructor
 public class AnswerRepositoryImpl implements AnswerRepositoryCustom {
@@ -39,16 +41,19 @@ public class AnswerRepositoryImpl implements AnswerRepositoryCustom {
     }
 
     public List<Long> findAllPreviousIdsBy(Long accountId, List<Long> projectIds, List<Question> questions) {
-        List<Long> questionIds = questions.stream()
-                                          .map(Question::getId)
-                                          .collect(Collectors.toList());
+        List<Long> questionItemIds = questions.stream()
+                                              .map(Question::getQuestionItem)
+                                              .map(QuestionItem::getId)
+                                              .collect(Collectors.toList());
 
         return queryFactory.select(answer.id.max())
                            .from(answer)
+                           .innerJoin(answer.question, question)
+                           .innerJoin(question.questionItem, questionItem)
                            .where(answer.account.id.eq(accountId),
                                   answer.project.id.in(projectIds),
-                                  answer.question.id.in(questionIds))
-                           .groupBy(answer.project, answer.question)
+                                  answer.question.questionItem.id.in(questionItemIds))
+                           .groupBy(answer.project, answer.question.questionItem)
                            .fetch();
 
     }
