acc9602f96c78d419c8f816cb33f7913b5613857
ragdoll <museopkim0214@gmail.com>
2021-05-31 14:29:02 +0900

    Feat: QuestionService - Answers와 연관된 Question Map을 반환하는 메서드 구현
    [#28]

--- src/main/java/com/careerzip/domain/question/service/QuestionService.java
+++ src/main/java/com/careerzip/domain/question/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.careerzip.domain.question.service;
 
+import com.careerzip.domain.archive.dto.request.createarchiverequest.CreateAnswerDetail;
 import com.careerzip.domain.archive.entity.Archive;
 import com.careerzip.domain.question.entity.Question;
 import com.careerzip.domain.question.repository.QuestionRepository;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +26,14 @@ public class QuestionService {
         QuestionPaper questionPaper = archive.getQuestionPaper();
         QuestionPaperForm questionPaperForm = questionPaper.getQuestionPaperForm();
         return questionRepository.findAllBy(questionPaperForm);
+    }
+
+    public Map<Long, Question> findAllMapBy(List<CreateAnswerDetail> archiveAnswers) {
+        List<Long> questionIds = archiveAnswers.stream()
+                                               .map(CreateAnswerDetail::getQuestionId)
+                                               .collect(Collectors.toList());
+        List<Question> questions = questionRepository.findAllByIds(questionIds);
+        return questions.stream()
+                        .collect(Collectors.toMap(Question::getId, Function.identity()));
     }
 }
--- src/test/java/com/careerzip/domain/question/service/QuestionServiceTest.java
+++ src/test/java/com/careerzip/domain/question/service/QuestionServiceTest.java
@@ -1,6 +1,8 @@
 package com.careerzip.domain.question.service;
 
 import com.careerzip.domain.answer.service.AnswerService;
+import com.careerzip.domain.archive.dto.request.createarchiverequest.CreateAnswerDetail;
+import com.careerzip.domain.archive.dto.request.createarchiverequest.CreateArchiveRequest;
 import com.careerzip.domain.archive.entity.Archive;
 import com.careerzip.domain.questionpaperform.entity.QuestionPaperForm;
 import com.careerzip.domain.question.entity.Question;
@@ -13,12 +15,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import static com.careerzip.testobject.answer.AnswerFactory.createAnswers;
-import static com.careerzip.testobject.archive.ArchiveFactory.createArchive;
+import static com.careerzip.testobject.answer.AnswerFactory.*;
+import static com.careerzip.testobject.archive.ArchiveFactory.*;
 import static com.careerzip.testobject.question.QuestionFactory.createQuestions;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,5 +52,24 @@ class QuestionServiceTest {
 
         // then
         assertThat(questions).usingRecursiveComparison().isEqualTo(testQuestions);
+    }
+
+    @Test
+    @DisplayName("Answer와 연관된 Question들을 가져와서 Map으로 만드는 테스트")
+    void findAllMapByAnswersTest() {
+        // given
+        List<Question> questions = createQuestions();
+        List<CreateAnswerDetail> answerDetails = createCreateAnswerDetailsOf(questions);
+
+        // when
+        when(questionRepository.findAllByIds(anyList())).thenReturn(questions);
+
+        Map<Long, Question> questionsMap = questionService.findAllMapBy(answerDetails);
+
+        // then
+        assertThat(questionsMap.keySet()).containsAll(questions.stream()
+                                                               .map(Question::getId)
+                                                               .collect(Collectors.toList()));
+        assertThat(questionsMap.size()).isEqualTo(questions.size());
     }
 }
