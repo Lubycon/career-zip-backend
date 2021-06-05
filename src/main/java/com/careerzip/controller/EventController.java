08c73e86d46b3bad430a581cf6cc709d4bb4982d
ragdoll <museopkim0214@gmail.com>
2021-06-05 15:02:17 +0900

    Feat: Link share count API 구현
    [#32]

--- src/main/java/com/careerzip/controller/EventController.java
+++ src/main/java/com/careerzip/controller/EventController.java
@@ -4,6 +4,7 @@ import com.careerzip.domain.event.service.EventService;
 import com.careerzip.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +14,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class EventController {
 
     private final EventService eventService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/share-link")
+    public ApiResponse<Long> shareCount() {
+        Long shareCount = eventService.shareCount();
+        return ApiResponse.success(shareCount);
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/share-link")
