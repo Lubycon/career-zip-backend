0074eb93ab35f6826eaf3cb2af604d1e1e937e90
ragdoll <museopkim0214@gmail.com>
2021-06-07 22:21:28 +0900

    Chore: Client Dev server CORS 설정 추가

--- src/main/java/com/careerzip/config/web/WebConfig.java
+++ src/main/java/com/careerzip/config/web/WebConfig.java
@@ -9,12 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final String CLIENT_URL = "https://career-zip.com";
+    private static final String DEV_URL = "https://dev.career-zip.com";
     private static final String CLIENT_LOCAL = "http://localhost:3000";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(CLIENT_URL, CLIENT_LOCAL)
+                .allowedOrigins(CLIENT_URL, DEV_URL, CLIENT_LOCAL)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
