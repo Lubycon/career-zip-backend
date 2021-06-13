e4e7d3bcb1379720c8e513784614a98c48407ae1
ragdoll <museopkim0214@gmail.com>
2021-06-13 16:38:41 +0900

    Fix: 리스트 Pageable에 Sort가 적용되지 않던 문제 수정

--- src/main/java/com/careerzip/domain/archive/repository/ArchiveRepositoryImpl.java
+++ src/main/java/com/careerzip/domain/archive/repository/ArchiveRepositoryImpl.java
@@ -3,12 +3,18 @@ package com.careerzip.domain.archive.repository;
 import com.careerzip.domain.account.entity.Account;
 import com.careerzip.domain.archive.entity.Archive;
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static com.careerzip.domain.archive.entity.QArchive.archive;
@@ -26,6 +32,8 @@ public class ArchiveRepositoryImpl implements ArchiveRepositoryCustom {
                             .innerJoin(archive.questionPaper, questionPaper).fetchJoin()
                             .innerJoin(questionPaper.questionPaperForm, questionPaperForm).fetchJoin()
                             .where(archive.account.eq(account))
+                            .orderBy(specifyOrder(pageable.getSort())
+                                                          .toArray(OrderSpecifier[]::new))
                             .offset(pageable.getOffset())
                             .limit(pageable.getPageSize())
                             .fetchResults();
@@ -38,5 +46,19 @@ public class ArchiveRepositoryImpl implements ArchiveRepositoryCustom {
                                                .innerJoin(questionPaper.questionPaperForm, questionPaperForm).fetchJoin()
                                                .where(archive.account.eq(account), archive.id.eq(archiveId))
                                                .fetchOne());
+    }
+
+    private List<OrderSpecifier> specifyOrder(Sort sort) {
+        String archive = "archive";
+        List<OrderSpecifier> orders = new ArrayList<>();
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String property = order.getProperty();
+            PathBuilder orderByExpression = new PathBuilder(Archive.class, archive);
+            orders.add(new OrderSpecifier(direction, orderByExpression.get(property)));
+        });
+
+        return orders;
     }
 }
