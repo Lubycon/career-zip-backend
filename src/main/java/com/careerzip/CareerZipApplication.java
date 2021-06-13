7e3e3f24f5934247d61800b2717d1f6a9d916bb7
ragdoll <museopkim0214@gmail.com>
2021-06-13 15:34:53 +0900

    Feat: Application 기본 Timezone 설정
    [#38]

--- src/main/java/com/careerzip/CareerZipApplication.java
+++ src/main/java/com/careerzip/CareerZipApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableConfigurationProperties({JwtProperties.class})
 @SpringBootApplication
 public class CareerZipApplication {
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CareerZipApplication.class, args);
