4892c8cb7990154153a3908d21286cbd7c6492af
ragdoll <museopkim0214@gmail.com>
2021-06-21 01:30:52 +0900

    Fix: Acquisition Entity - 기본 생성가 추가
    [#39]

--- src/main/java/com/careerzip/domain/acquisition/Acquisition.java
+++ src/main/java/com/careerzip/domain/acquisition/Acquisition.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Acquisition {
