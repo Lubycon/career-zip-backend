c980f0cf0a3e4980a8e3db68d807168b34477b77
ragdoll <museopkim0214@gmail.com>
2021-06-27 04:21:38 +0900

    Fix: 불필요한 로그 삭제
    [#42]

--- src/main/java/com/careerzip/security/admin/FormLoginService.java
+++ src/main/java/com/careerzip/security/admin/FormLoginService.java
@@ -1,11 +1,9 @@
 package com.careerzip.security.admin;
 
-import com.careerzip.domain.account.entity.Role;
 import com.careerzip.global.admin.entity.AdminAccount;
 import com.careerzip.global.admin.repository.AdminAccountRepository;
 import com.careerzip.global.error.exception.entity.AccountNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class FormLoginService implements UserDetailsService {
@@ -24,8 +21,6 @@ public class FormLoginService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String adminName) throws UsernameNotFoundException {
-        log.info(">>>>>>>>>>>>>>>>> Come>>>>>>>>>>>>>");
-        log.info(">>>>>>>>>>>>>>>>>>>>> {}", adminName);
         AdminAccount adminAccount = adminAccountRepository.findByName(adminName)
                                                           .orElseThrow(AccountNotFoundException::new);
         return new User(adminAccount.getName(), adminAccount.getPassword(),
