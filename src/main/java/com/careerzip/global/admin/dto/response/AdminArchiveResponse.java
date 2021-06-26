e80471b8de65e1098d6002dfa404b0af56ae6019
ragdoll <museopkim0214@gmail.com>
2021-06-26 22:27:05 +0900

    Fix: 기존의 로직을 사용하기 위해 Archive 상세 조회 DTO 변경
    [#42]

--- src/main/java/com/careerzip/global/admin/dto/response/AdminArchiveResponse.java
+++ src/main/java/com/careerzip/global/admin/dto/response/AdminArchiveResponse.java
@@ -1,34 +1,50 @@
 package com.careerzip.global.admin.dto.response;
 
+import com.careerzip.domain.account.dto.response.AccountSummary;
 import com.careerzip.domain.account.entity.Account;
+import com.careerzip.domain.archive.dto.response.archivedetailresponse.ProjectSummary;
 import com.careerzip.domain.archive.dto.response.archivedetailresponse.QuestionWithAnswers;
-import com.careerzip.domain.archive.dto.response.archivesresponse.RelatedProject;
 import com.careerzip.domain.archive.entity.Archive;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 public class AdminArchiveResponse {
 
     @NotNull
-    private final ArchiveRelatedData archive;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 MM월 dd일 HH시 mm분")
+    private final LocalDateTime createdDateTime;
+
+    @NotNull
+    private final Set<ProjectSummary> projects;
+
+    @NotNull
+    private final AccountSummary account;
 
     @NotNull
     private final List<QuestionWithAnswers> questionWithAnswers;
 
     @Builder
-    private AdminArchiveResponse(ArchiveRelatedData archive, List<QuestionWithAnswers> questionWithAnswers) {
-        this.archive = archive;
+    private AdminArchiveResponse(LocalDateTime createdDateTime, Set<ProjectSummary> projects, AccountSummary account,
+                                 List<QuestionWithAnswers> questionWithAnswers) {
+        this.createdDateTime = createdDateTime;
+        this.projects = projects;
+        this.account = account;
         this.questionWithAnswers = questionWithAnswers;
     }
 
-    public static AdminArchiveResponse of(Archive archive, List<RelatedProject> projects, Account account,
+    public static AdminArchiveResponse of(Archive archive, Set<ProjectSummary> projects, Account account,
                                           List<QuestionWithAnswers> questionWithAnswers) {
         return AdminArchiveResponse.builder()
-                                   .archive(ArchiveRelatedData.of(archive, projects, account))
+                                   .createdDateTime(archive.getCreatedDateTime())
+                                   .projects(projects)
+                                   .account(AccountSummary.from(account))
                                    .questionWithAnswers(questionWithAnswers)
                                    .build();
     }
