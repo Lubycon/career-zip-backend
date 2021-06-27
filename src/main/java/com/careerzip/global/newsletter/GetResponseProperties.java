e0dc2527fb91a950d8a335555d5b09bf4aaa2274
ragdoll <museopkim0214@gmail.com>
2021-06-27 17:23:24 +0900

    Feat: GetResponse properties 빈 생성
    [#43]

---
+++ src/main/java/com/careerzip/global/newsletter/GetResponseProperties.java
@@ -0,0 +1,16 @@
+package com.careerzip.global.newsletter;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@RequiredArgsConstructor
+@Getter
+@ConstructorBinding
+@ConfigurationProperties(prefix = "getresponse")
+public class GetResponseProperties {
+
+    private final String baseUrl;
+    private final String apiKey;
+}
