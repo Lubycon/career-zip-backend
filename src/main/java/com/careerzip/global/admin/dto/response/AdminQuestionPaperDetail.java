8be5f2a1c77fc06296a4a7f1e60832e5b6f94233
ragdoll <museopkim0214@gmail.com>
2021-06-28 20:28:47 +0900

    Feat: 질문지 조회 DTO 추가
    [#48]

---
+++ src/main/java/com/careerzip/global/admin/dto/response/AdminQuestionPaperDetail.java
@@ -0,0 +1,48 @@
+package com.careerzip.global.admin.dto.response;
+
+import com.careerzip.domain.questionpaper.entity.QuestionPaper;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class AdminQuestionPaperDetail {
+
+    private final long id;
+
+    private final String title;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDateTime startDateTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDateTime endDateTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDateTime finishDateTime;
+
+    private final boolean opened;
+
+    @Builder
+    private AdminQuestionPaperDetail(long id, String title, LocalDateTime startDateTime, LocalDateTime endDateTime,
+                                     LocalDateTime finishDateTime, boolean opened) {
+        this.id = id;
+        this.title = title;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.finishDateTime = finishDateTime;
+        this.opened = opened;
+    }
+
+    public static AdminQuestionPaperDetail from(QuestionPaper questionPaper) {
+        return AdminQuestionPaperDetail.builder()
+                                       .id(questionPaper.getId())
+                                       .title(questionPaper.getTitle())
+                                       .startDateTime(questionPaper.getStartDateTime())
+                                       .endDateTime(questionPaper.getEndDateTime())
+                                       .opened(questionPaper.getOpened())
+                                       .build();
+    }
+}
