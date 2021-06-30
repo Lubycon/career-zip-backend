b93fed3e39f5e0d69147da084fc8f40b3211e6a4
ragdoll <museopkim0214@gmail.com>
2021-06-30 22:51:54 +0900

    Feat: QuestionPaper 수정 DTO 추가
    [#50]

---
+++ src/main/java/com/careerzip/global/admin/dto/request/UpdateQuestionPaperRequest.java
@@ -0,0 +1,33 @@
+package com.careerzip.global.admin.dto.request;
+
+import com.careerzip.domain.questionpaper.entity.QuestionPaper;
+import com.careerzip.domain.questionpaperform.entity.QuestionPaperForm;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+public class UpdateQuestionPaperRequest {
+
+    private String title;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDate finishDate;
+
+    public QuestionPaper toEntity(QuestionPaperForm questionPaperForm) {
+        LocalTime startTime = LocalTime.of(0, 0, 0);
+        LocalTime endTime = LocalTime.of(23, 59, 59);
+        LocalTime finishTime = LocalTime.of(23, 55, 0);
+
+        return QuestionPaper.builder()
+                            .title(title)
+                            .startDateTime(LocalDateTime.of(startDate, startTime))
+                            .endDateTime(LocalDateTime.of(endDate, endTime))
+                            .finishDateTime(LocalDateTime.of(finishDate, finishTime))
+                            .opened(true)
+                            .questionPaperForm(questionPaperForm)
+                            .build();
+    }
+}
