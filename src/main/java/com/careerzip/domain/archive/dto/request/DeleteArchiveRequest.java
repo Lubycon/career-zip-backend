df5968b7ca88328daba11d09ec974a8f792f1ce8
chobo-dev <rhtjdals0814@gmail.com>
2021-12-23 14:39:26 +0900

    Add all args constructor

--- src/main/java/com/careerzip/domain/archive/dto/request/DeleteArchiveRequest.java
+++ src/main/java/com/careerzip/domain/archive/dto/request/DeleteArchiveRequest.java
@@ -1,10 +1,12 @@
 package com.careerzip.domain.archive.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 import java.util.List;
 
 @Value
+@AllArgsConstructor
 public class DeleteArchiveRequest {
     private List<Long> deleteArchiveIds;
 }
